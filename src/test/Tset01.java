f22b4509fbad647738540141d23a7490ac650caa
pengsc <pengshichaung03@163.com>
2021-10-19 15:15:33 +0800

    master分支第一次提交

---
+++ src/test/Tset01.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Author pengsc
+ * @Date 2021/10/19 15:13
+ * @Version 1.0
+ */
+public class Tset01 {
+    public static void main(String[] args) {
+        System.out.println("master分支第一次提交");
+    }
+}
